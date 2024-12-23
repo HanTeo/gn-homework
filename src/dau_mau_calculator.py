0fae6749987eb6af294ea6e260c077202df76024
HanTeo <han.wei.teo@avios.com>
2024-12-24 00:49:39 +0100

    add mau test

--- src/dau_mau_calculator.py
+++ src/dau_mau_calculator.py
@@ -2,19 +2,15 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, to_date, date_format, approx_count_distinct
 
 
-def preprocess_data(df):
-    df = df.withColumn("date", to_date(col("timestamp")))
-    df = df.withColumn("month", date_format(col("timestamp"), "yyyy-MM"))
-    return df
-
-
 def calculate_dau(df):
+    df = df.withColumn("date", to_date(col("timestamp")))
     return df.groupBy("date").agg(
         approx_count_distinct("user_id").alias("daily_active_users")
     )
 
 
 def calculate_mau(df):
+    df = df.withColumn("month", date_format(col("timestamp"), "yyyy-MM"))
     return df.groupBy("month").agg(
         approx_count_distinct("user_id").alias("monthly_active_users")
     )
@@ -24,8 +20,6 @@ if __name__ == "__main__":
     spark = SparkSession.builder.appName("DAU_MAU_Calculation").getOrCreate()
 
     df = spark.read.parquet("data/user_interactions_sample.parquet")
-
-    df = preprocess_data(spark)
 
     dau = calculate_dau(df)
     print("Daily Active Users (DAU):")
--- tests/test_dau_mau_calculator.py
+++ tests/test_dau_mau_calculator.py
@@ -2,8 +2,7 @@ import pytest
 from pyspark.sql import SparkSession
 from chispa.dataframe_comparer import assert_df_equality
 from datetime import datetime, date
-from src.dau_mau_calculator import calculate_dau, preprocess_data
-from pyspark.sql.functions import col, to_date, date_format, approx_count_distinct
+from src.dau_mau_calculator import calculate_dau, calculate_mau
 
 
 @pytest.fixture(scope="session")
@@ -25,12 +24,33 @@ def test_calculate_dau(spark):
 
     df = spark.createDataFrame(input_data, ["user_id", "timestamp"])
 
-    df = preprocess_data(df)
-
     result_df = calculate_dau(df)
 
     expected_data = [(date(2023, 1, 1), 2), (date(2023, 1, 2), 2)]
     expected_df = spark.createDataFrame(expected_data, ["date", "daily_active_users"])
+
+    assert_df_equality(
+        expected_df,
+        result_df,
+        ignore_nullable=True,
+    )
+
+
+def test_calculate_mau(spark):
+    input_data = [
+        ("u000001", datetime(2023, 1, 1, 10, 0, 0)),
+        ("u000002", datetime(2023, 1, 1, 11, 0, 0)),
+        ("u000001", datetime(2023, 2, 2, 9, 30, 0)),
+        ("u000003", datetime(2023, 2, 4, 15, 0, 0)),
+        ("u000002", datetime(2023, 2, 4, 23, 0, 0)),
+    ]
+
+    df = spark.createDataFrame(input_data, ["user_id", "timestamp"])
+
+    result_df = calculate_mau(df)
+
+    expected_data = [("2023-01", 2), ("2023-02", 3)]
+    expected_df = spark.createDataFrame(expected_data, ["month", "monthly_active_users"])
 
     assert_df_equality(
         expected_df,
